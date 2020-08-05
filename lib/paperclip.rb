ce84dbff1f9cb8ae02f35fed23362eb46040c2ad
Brendon Muir <brendon@spike.net.nz>
2020-08-05 20:28:30 +1200

    Add `validation_errors_to` option - customize copying of errors (#26)
    * Introduce `add_validation_errors_to` option
    
    Valid values for `add_validation_errors_to` are `:attribute`, `:base`,
    or `:both`.
    
    The option is globally set to `:both` that retains the existing
    behavior of adding validation errors to both the attribute and the
    model base.

--- lib/paperclip.rb
+++ lib/paperclip.rb
@@ -98,7 +98,8 @@ module Paperclip
       swallow_stderr: true,
       use_exif_orientation: true,
       whiny: true,
-      is_windows: Gem.win_platform?
+      is_windows: Gem.win_platform?,
+      add_validation_errors_to: :both
     }
   end
 
