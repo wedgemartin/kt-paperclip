e42c195d2db73454d58836cef1f7ab0507e20481
Wedge Martin <wedge@shutdown.com>
2023-11-06 12:31:11 -0800

    fix :on issue..

--- lib/paperclip/has_attached_file.rb
+++ lib/paperclip/has_attached_file.rb
@@ -91,13 +91,13 @@ module Paperclip
       name = @name
       @klass.send(:after_save) { send(name).send(:save) }
       @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
-      if @klass.respond_to?(:after_commit)
-        @klass.send(:after_commit, on: :destroy) do
-          send(name).send(:flush_deletes)
-        end
-      else
+      # if @klass.respond_to?(:after_commit)
+        # @klass.send(:after_commit, on: :destroy) do
+          # send(name).send(:flush_deletes)
+        # end
+      # else
         @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
-      end
+      # end
     end
 
     def add_paperclip_callbacks
